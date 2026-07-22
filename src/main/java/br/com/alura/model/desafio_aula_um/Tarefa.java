d7c3c4980181bf8263b417c5dc5a875d6dd8d36f
candasilva_meli <caique.ndasilva@mercadolivre.com>
2026-07-22 20:56:33 -0300

    Iniciando desafio 1

---
+++ src/main/java/br/com/alura/model/desafio_aula_um/Tarefa.java
@@ -0,0 +1,31 @@
+package br.com.alura.model.desafio_aula_um;
+
+public class Tarefa {
+    private String descricao;
+    private boolean concluida;
+    private String pessoaResponsavel;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isConcluida() {
+        return concluida;
+    }
+
+    public void setConcluida(boolean concluida) {
+        this.concluida = concluida;
+    }
+
+    public String getPessoaResponsavel() {
+        return pessoaResponsavel;
+    }
+
+    public void setPessoaResponsavel(String pessoaResponsavel) {
+        this.pessoaResponsavel = pessoaResponsavel;
+    }
+}
